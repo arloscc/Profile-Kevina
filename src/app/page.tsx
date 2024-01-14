d6b658b252864a5fddba8b9c4bb71c3a982209f7
arloscc <130179466+arloscc@users.noreply.github.com>
2024-01-14 15:09:09 +0700

    finishing

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Nav from "./Nav/page"
+import Hero from "./Hero/page"
+import Skills from "./Skills/page"
+
+
+export default function Page(){
+  return(
+    <>
+    
+    <Hero />
+    <Skills />
+
+    
+    
+    </>
+  )
+}
